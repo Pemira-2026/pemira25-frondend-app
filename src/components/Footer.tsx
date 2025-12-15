0ef09006d04ae001ff7bf57b8f936f098a61c119
Tayen15 <tiantiantitik@gmail.com>
2025-12-16 01:47:36 +0700

    feat: add new Footer component with copyright and social media links

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -6,7 +6,8 @@ export default function Footer() {
                <div className="container mx-auto px-4 flex flex-col md:flex-row items-center justify-between gap-4 text-sm text-slate-500">
                     <p>&copy; 2025 IT Support PEMIRA STT Terpadu <a href="https://nurulfikri.ac.id" className="hover:underline-offset-1 hover:underline transition-colors" target="_blank" rel="noopener noreferrer">Nurul Fikri</a>.</p>
                     <div className="flex items-center gap-6">
-                         <Link href="https://www.instagram.com/pemirasttnf/" className="hover:text-primary transition-colors">Instagram</Link>
+                         <Link href="https://www.instagram.com/pemirasttnf/" target="_blank" rel="noopener noreferrer" className="hover:text-primary transition-colors">Instagram</Link>
+                         <Link href="https://www.youtube.com/@KPRSTTNF" target="_blank" rel="noopener noreferrer" className="hover:text-primary transition-colors">Youtube</Link>
                     </div>
                </div>
           </footer>
