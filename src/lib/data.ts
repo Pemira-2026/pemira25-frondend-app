219957d94de203401c435d1d73480a95207d9cc6
Tayen15 <tiantiantitik@gmail.com>
2026-01-08 13:39:51 +0700

    ref: comment data timeline

--- src/lib/data.ts
+++ src/lib/data.ts
@@ -45,11 +45,11 @@ export const candidates = [
 ];
 
 export const timeline = [
-  { date: "7 - 12 Jan 2026", event: "Pendaftaran Paslon" },
-  { date: "24 - 25 Jan 2026", event: "Verifikasi Berkas" },
-  { date: "28 Jan - 10 Feb 2026", event: "Kampanye Terbuka" },
-  { date: "16 - 18 Feb 2026", event: "Pemungutan Suara" },
-  { date: "28 Feb 2026", event: "Pelantikan" }
+  // { date: "7 - 12 Jan 2026", event: "Pendaftaran Paslon" },
+  // { date: "24 - 25 Jan 2026", event: "Verifikasi Berkas" },
+  // { date: "28 Jan - 10 Feb 2026", event: "Kampanye Terbuka" },
+  // { date: "16 - 18 Feb 2026", event: "Pemungutan Suara" },
+  // { date: "28 Feb 2026", event: "Pelantikan" }
 ];
 
 export const tutorialSteps = [
